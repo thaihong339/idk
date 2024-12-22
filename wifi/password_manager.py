cf72946add482d6054ad177395aed60dd7fe4bc7
thaihong339 <thaivuhong09@gmail.com>
2024-12-22 16:13:57 +0700

    Update password_manager.py

--- wifi/password_manager.py
+++ wifi/password_manager.py
@@ -18,17 +18,26 @@ def attempt_multiple_passwords(ssid, passwords):
         thread.join()
 
 def read_passwords_from_file(file_path):
-    with open(file_path, 'r') as file:
-        passwords = file.readlines()
-    return [pwd.strip() for pwd in passwords]
+    try:
+        with open(file_path, 'r') as file:
+            passwords = file.readlines()
+        return [pwd.strip() for pwd in passwords]
+    except FileNotFoundError:
+        print(f"File {file_path} not found.")
+        return []
 
 def main():
     print("Wi-Fi Password Manager")
     ssid = input("Enter the SSID of the Wi-Fi network: ")
-    file_path = input("Enter the path to the password file: ")
+    file_path = input("Enter the path to the password file (default: password.txt): ")
+    if not file_path:
+        file_path = 'wordlists-vn1m (1).txt'
     passwords = read_passwords_from_file(file_path)
     
-    attempt_multiple_passwords(ssid, passwords)
+    if passwords:
+        attempt_multiple_passwords(ssid, passwords)
+    else:
+        print("No passwords to attempt.")
 
 if __name__ == "__main__":
     main()
